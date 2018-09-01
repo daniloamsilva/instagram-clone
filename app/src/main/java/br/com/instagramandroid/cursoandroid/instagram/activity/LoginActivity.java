c264b24a72458bb715945eaded28cc8a7d8e1c21
daniloamsilva <danilo_dams@yahoo.com.br>
2018-09-01 20:47:35 -0300

    Aula 261 - Criando Interface de Login - Parte 2

---
+++ app/src/main/java/br/com/instagramandroid/cursoandroid/instagram/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package br.com.instagramandroid.cursoandroid.instagram.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.instagramandroid.cursoandroid.instagram.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
