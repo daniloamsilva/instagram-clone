534bf819c4725d2d157ab8f388ea9c0d466954ee
daniloamsilva <danilo_dams@yahoo.com.br>
2018-09-03 20:33:51 -0300

    Aula 265 - Cadastro de Usuários - Parte 1

--- app/src/main/java/br/com/instagramandroid/cursoandroid/instagram/activity/CadastroActivity.java
+++ app/src/main/java/br/com/instagramandroid/cursoandroid/instagram/activity/CadastroActivity.java
@@ -3,15 +3,63 @@ package br.com.instagramandroid.cursoandroid.instagram.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import br.com.instagramandroid.cursoandroid.instagram.R;
+import br.com.instagramandroid.cursoandroid.instagram.util.ParseErros;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText textoUsuario;
+    private EditText textoEmail;
+    private EditText textoSenha;
+    private Button botaoCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        textoUsuario    =  findViewById(R.id.text_usuario);
+        textoEmail      =  findViewById(R.id.text_email);
+        textoSenha      =  findViewById(R.id.text_senha);
+        botaoCadastrar  =  findViewById(R.id.button_cadastrar);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cadastrarUsuario();
+            }
+        });
+
+    }
+
+    private void cadastrarUsuario(){
+        // Criação do objeto Usuario
+        ParseUser usuario = new ParseUser();
+        usuario.setUsername(textoUsuario.getText().toString());
+        usuario.setEmail(textoEmail.getText().toString());
+        usuario.setPassword(textoSenha.getText().toString());
+
+        // Salvar dados do usuário
+        usuario.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){ // sucesso ao salvar
+                    Toast.makeText(CadastroActivity.this, "Cadastro feito com sucesso!", Toast.LENGTH_SHORT).show();
+                }else{ // erro ao salvar
+                    ParseErros parseErros = new ParseErros();
+                    String erro = parseErros.getErro(e.getCode());
+                    Toast.makeText(CadastroActivity.this, erro, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void abrirLoginUsuario(View view){
