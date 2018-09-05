1a889145a60e9c78675273a04d87814fbb6d4117
daniloamsilva <danilo_dams@yahoo.com.br>
2018-09-04 23:44:28 -0300

    Aula 271 - Criando uma Toolbar

--- app/src/main/java/br/com/instagramandroid/cursoandroid/instagram/activity/MainActivity.java
+++ app/src/main/java/br/com/instagramandroid/cursoandroid/instagram/activity/MainActivity.java
@@ -2,7 +2,10 @@ package br.com.instagramandroid.cursoandroid.instagram.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -20,12 +23,23 @@ import br.com.instagramandroid.cursoandroid.instagram.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbarPrincipal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbarPrincipal = findViewById(R.id.toolbar_principal);
+        toolbarPrincipal.setLogo(R.drawable.instagramlogo);
+        setSupportActionBar(toolbarPrincipal);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
